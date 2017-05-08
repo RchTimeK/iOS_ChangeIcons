a16d7f56399971c027d2adebea4f02d269de3c61
MBRB <mbrb@MBRBdeMac-mini.local>
2017-05-08 14:36:52 +0800

    初始化

---
+++ RCChangeIcons/RCChangeIcons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RCChangeIcons
+//
+//  Created by RongCheng on 2017/5/8.
+//  Copyright © 2017年 MBRB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RCChangeIcons/RCChangeIcons/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  RCChangeIcons
+//
+//  Created by RongCheng on 2017/5/8.
+//  Copyright © 2017年 MBRB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"--你好世界和平--");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
