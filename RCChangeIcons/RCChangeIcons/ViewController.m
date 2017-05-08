a5b9ecc7260c62df98605252076f45e677fbf1d1
MBRB <mbrb@MBRBdeMac-mini.local>
2017-05-08 16:01:27 +0800

    切换appIcon

--- RCChangeIcons/RCChangeIcons/ViewController.m
+++ RCChangeIcons/RCChangeIcons/ViewController.m
@@ -16,9 +16,41 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"--你好世界和平--");
+    NSLog(@"--World Peace--");
 }
-
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [super touchesBegan:touches withEvent:event];
+    [self changeAppIcons];
+}
+- (void)changeAppIcons
+{
+    if ([UIApplication sharedApplication].supportsAlternateIcons) {
+        NSLog(@"you can change this app's icon");
+    }else{
+        NSLog(@"you can not change this app's icon");
+        return;
+    }
+    
+    NSString *iconName = [[UIApplication sharedApplication] alternateIconName];
+    
+    if (iconName) {
+        [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"set icon error: %@",error);
+            }else{
+                NSLog(@"Set up successfully,Press the Home key to view");
+            }
+        }];
+    }else{
+        [[UIApplication sharedApplication] setAlternateIconName:@"newIcon" completionHandler:^(NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"set icon error: %@",error);
+            }else{
+                NSLog(@"Set up successfully,Press the Home key to view");
+            }
+        }];
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
